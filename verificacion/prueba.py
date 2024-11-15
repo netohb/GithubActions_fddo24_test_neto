bd95f0603c0f3ee12c951f81141d33b7d7c5e59a
albertomarquez794 <thebrayanxd45@gmail.com>
2024-11-15 16:24:30 -0600

    Lol68

--- verificacion/prueba.py
+++ verificacion/prueba.py
@@ -16,6 +16,5 @@ numero2 = float(input("Ingresa el segundo número: "))
 
 operacion = Operaciones(numero1, numero2)
 
-printf("fdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")
 print(f"La suma de {numero1} y {numero2} es: {operacion.sumar()}")
 print(f"La resta de {numero1} y {numero2} es: {operacion.restar()}")
