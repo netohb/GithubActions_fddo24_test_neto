4e6f3a3996e69d4a27a125885cbe90e57062e444
albertomarquez794 <thebrayanxd45@gmail.com>
2024-11-14 19:38:06 -0600

    Agregando la prueba

---
+++ verificacion/prueba.py
@@ -0,0 +1,21 @@
+class Operaciones:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def sumar(self):
+        return self.a + self.b
+
+    def restar(self):
+        return self.a - self.b
+
+
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+
+operacion = Operaciones(numero1, numero2)
+
+
+print(f"La suma de {numero1} y {numero2} es: {operacion.sumar()}")
+print(f"La resta de {numero1} y {numero2} es: {operacion.restar()}")
